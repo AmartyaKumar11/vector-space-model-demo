563add8390f309c438fa6ecdc8614ab9f7e0859c
Amartya Kumar <kumaramartya11@gmail.com>
2026-03-30 10:46:06 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vector-space-model-demo/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
